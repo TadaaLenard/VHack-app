8c4ec7423a3bf764ed9436fc3978e974c9ffee3b
chew0522 <dcbc0123456789@gmail.com>
2025-03-23 09:34:04 +0800

    Feature Box component
    green box component such as soduku in cognitive exercise feature for further usage

--- lib/Memory Retention/MemoryRetentionHomePage.dart
+++ lib/Memory Retention/MemoryRetentionHomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vhackapp/Component/leading.dart';
 import 'ThreeWordsAhead.dart';
+import 'package:vhackapp/Component/FeatureBox.dart';
 
 class MemoryRetentionHomePage extends StatelessWidget {
   const MemoryRetentionHomePage({super.key});
@@ -13,8 +14,10 @@ class MemoryRetentionHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: const [
-            RectangleBox(text: "3 Words Ahead Games"),
-            SizedBox(height: 16),
+            FeatureBox(
+              text: "3 Words Ahead Games",
+              destination: const ThreeWordsAhead(), // Pass destination
+            ),
             
           ],
         ),
@@ -23,45 +26,3 @@ class MemoryRetentionHomePage extends StatelessWidget {
   }
 }
 
-class RectangleBox extends StatelessWidget {
-  final String text;
-  const RectangleBox({super.key, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (text == "3 Words Ahead Games") {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const ThreeWordsAhead()),
-          );
-        }
-      },
-      child: Container(
-        width: 358, 
-          height: 110, 
-          margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 8), // Adds margin
-          decoration: BoxDecoration(
-            color: const Color(0xFFB7EFC5),
-            borderRadius: BorderRadius.circular(16), 
-          ),
-          alignment: Alignment.center,
-          child: Align(
-          alignment: Alignment.bottomLeft, // Aligns text to the bottom left
-          child: Padding(
-            padding: const EdgeInsets.all(12.0), // Adds padding from the edges
-            child: Text(
-              text,
-              style: const TextStyle(
-                color: Colors.black, 
-                fontSize: 18, 
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      )
-    );
-  }
-}
